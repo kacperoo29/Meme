4660f2649539acb21dd545345c1515626ea7d822
Kacper Bajeński <erednay29@gmail.com>
2019-06-04 15:18:27 +0200

    workung?

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,54 @@ workspace "Meme"
 	
 outputdir = "%{cfg.buildcfg}-%{cfg.system}"
 
+project "GLFW"
+	location "Meme/3rdparty/GLFW"
+	kind "StaticLib"
+	language "C"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"Meme/3rdparty/GLFW/include/GLFW/glfw3.h",
+        "Meme/3rdparty/GLFW/include/GLFW/glfw3native.h",
+        "Meme/3rdparty/GLFW/src/glfw_config.h",
+        "Meme/3rdparty/GLFW/src/context.c",
+        "Meme/3rdparty/GLFW/src/init.c",
+        "Meme/3rdparty/GLFW/src/input.c",
+        "Meme/3rdparty/GLFW/src/monitor.c",
+        "Meme/3rdparty/GLFW/src/vulkan.c",
+        "Meme/3rdparty/GLFW/src/window.c"
+	}
+	
+	filter "system:windows"
+		buildoptions { "-std=c11", "-lgdi32" }
+		systemversion "latest"
+		staticruntime "On"
+		files
+        {
+           "Meme/3rdparty/GLFW/src/win32_init.c",
+           "Meme/3rdparty/GLFW/src/win32_joystick.c",
+           "Meme/3rdparty/GLFW/src/win32_monitor.c",
+           "Meme/3rdparty/GLFW/src/win32_time.c",
+           "Meme/3rdparty/GLFW/src/win32_thread.c",
+           "Meme/3rdparty/GLFW/src/win32_window.c",
+           "Meme/3rdparty/GLFW/src/wgl_context.c",
+           "Meme/3rdparty/GLFW/src/egl_context.c",
+           "Meme/3rdparty/GLFW/src/osmesa_context.c"
+        }
+		
+		defines 
+		{ 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+		}
+		
+	filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
+		
+
 project "Meme"
 	location "Meme"
 	kind "SharedLib"
@@ -30,7 +78,15 @@ project "Meme"
 	includedirs 
 	{
 		"%{prj.name}/3rdparty/spdlog/include",
-		"Meme/src"
+		"%{prj.name}/3rdparty/glfw/include",
+		"Meme/src",
+		"Meme/3rdparty/GLFW/include"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
 	}
 
 	filter "system:windows"
@@ -54,6 +110,7 @@ project "Meme"
 	filter "configurations:Dist"
 		defines "__DIST"
 		optimize "On"
+		
 
 project "Launcher"
 	location "launcher"
